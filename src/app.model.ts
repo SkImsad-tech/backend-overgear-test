a7d5464b3fc385d8ce5e46675729a5d792a0c0dc
Atlantis <SkImsad@gmail.com>
2021-03-11 11:44:48 +0500

    fix: updated typeorm connection

--- src/app.model.ts
+++ src/app.model.ts
@@ -1,30 +1,22 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppProvider } from './app.provider'
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, } from '@nestjs/typeorm';
+import { getConnectionOptions, Connection } from 'typeorm'
 
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: "mysql",
-            host: "mysql",
-            port: 3306,
-            username: "root",
-            password: "123698741",
-            database: "test",
-            entities: [
-                Account,
-                User,
-                Transaction
-            ],
-            synchronize: true,
-            logging: false,
-            retryAttempts: 10,
-            retryDelay: 1000
+        TypeOrmModule.forRootAsync({
+            useFactory: async () =>
+            Object.assign(await getConnectionOptions(), {
+              autoLoadEntities: true,
+            }),
         })
     ],
     controllers: [AppController],
     providers: [AppProvider],
 })
 
-export class AppModule {}
+export class AppModule {
+    constructor(private connection: Connection) {}
+}
